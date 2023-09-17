5c7bf3eb42219129232362dc20098ec5b65aece2
Tanakorn <winwin6d@hotmail.com>
2023-09-18 02:15:32 +0700

    add is_published and end_date, also modified to used it

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -13,14 +13,30 @@ class Question(models.Model):
     Contain __str__ method for string representation.
     """
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('date published', default=timezone.now)
+    end_date = models.DateTimeField('date ended', default=None, blank=True, null=True)
 
     def __str__(self) -> str:
         return self.question_text
 
     def was_published_recently(self):
+        """Method return boolean, either the question published within a day or not"""
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
+    def can_vote(self):
+        """Method return true if current time is in voting period."""
+        now = timezone.now()
+        if self.end_date is None:
+            return True
+        if self.pub_date <= now < self.end_date:
+            return True
+
+    def is_published(self):
+        """Method return true if question was published on or after current date and time."""
+        now = timezone.now()
+        if self.pub_date < now:
+            return True
 
 
 class Choice(models.Model):
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,7 +6,7 @@ from django.template import loader
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
-from .models import Question
+from .models import Question, Choice
 
 
 # Create your views here.
