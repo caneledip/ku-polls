fa51c0f3470cef3879107e7f9fc7de3a46f7cc79
Canele <winwin6d@hotmail.com>
2024-09-08 21:48:42 +0700

    add descriptive message after voting or access unavailable poll, poll show previous vote from user

--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,13 @@
 """Module for polls application view"""
 from django.contrib.auth.decorators import login_required
-from django.db.models import F
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render,redirect
 from django.urls import reverse
 from django.utils import timezone
 from django.views import generic
+from django.contrib import messages
 
-from .models import Choice, Question
+from .models import Choice, Question, Vote
 
 
 class IndexView(generic.ListView):
@@ -37,6 +37,24 @@ class DetailView(generic.DetailView):
         Excludes any questions that aren't published yet.
         """
         return Question.objects.filter(pub_date__lte=timezone.now())
+    
+    def get(self, request, *args, **kwargs):
+        question = self.get_object()
+
+        # Can you vote on this question?
+        if not question.can_vote():
+            messages.error(request, "Voting is not allowed for this question.")
+            return redirect('polls:index')
+
+        # Check for User vote on this question.
+        user_vote = None
+        if request.user.is_authenticated:
+            user_vote = Vote.objects.filter(user=request.user, choice__question=question).first()
+
+        return render(request, self.template_name, {
+            'question': question,
+            'previous_vote': user_vote,  # Pass the user's previous vote to the template
+        })
 
 
 class ResultsView(generic.DetailView):
@@ -53,10 +71,10 @@ def vote(request, question_id):
     user to index page. If poll is available, user can vote
     on poll's choice and submit it.
     """
-    # comment or delete this part after developped
-    user = request.user
-    print("current user is: ", user.id, "login", user.username)
-    print("Real name: ", user.first_name)
+
+    this_user = request.user
+    print("current user is: ", this_user.id, "login", this_user.username)
+    print("Real name: ", this_user.first_name)
     question = get_object_or_404(Question, pk=question_id)
 
     if not question.can_vote():
@@ -75,8 +93,17 @@ def vote(request, question_id):
             },
         )
 
-    selected_choice.votes = F("votes") + 1
-    selected_choice.save()
+    try:
+        # find a vote of this user for this question.
+        vote = Vote.objects.get(user=this_user, choice__question=question)
+        vote.choice = selected_choice
+        messages.success(request, f'Your Vote for "{question.question_text}" have been updated to "{selected_choice.choice_text}".')
+    except Vote.DoesNotExist:
+        vote = Vote(user=this_user, choice=selected_choice)
+        messages.success(request,f"Vote for '{selected_choice.choice_text}' success.")
+
+    # save change to new vote or existing vote.
+    vote.save()
     # Always return an HttpResponseRedirect after successfully dealing
     # with POST data. This prevents data from being posted twice if a
     # user hits the Back button.
